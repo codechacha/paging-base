6d5af5e32e0ed522fa228386f1a111f5e0238d43
Jeongsik Mun <farfs.dev@gmail.com>
2018-12-26 09:06:51 +0900

    first commit

---
+++ app/src/main/java/com/jsandroid/paging/data/GithubRepository.kt
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.jsandroid.paging.data
+
+import android.content.Context
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import com.jsandroid.paging.model.RepoSearchResult
+import com.jsandroid.paging.api.GithubService
+import com.jsandroid.paging.api.searchRepos
+import com.jsandroid.paging.db.GithubLocalCache
+import com.jsandroid.paging.db.RepoDatabase
+
+/**
+ * Repository class that works with local and remote data sources.
+ */
+class GithubRepository(context: Context) {
+
+    private val service: GithubService = GithubService.create()
+    private val cache: GithubLocalCache = GithubLocalCache(context)
+
+    // keep the last requested page. When the request is successful, increment the page number.
+    private var lastRequestedPage = 1
+
+    // LiveData of network errors.
+    private val networkErrors = MutableLiveData<String>()
+
+    // avoid triggering multiple requests in the same time
+    private var isRequestInProgress = false
+
+    /**
+     * Search repositories whose names match the query.
+     */
+    fun search(query: String): RepoSearchResult {
+        Log.d("GithubRepository", "New query: $query")
+        lastRequestedPage = 1
+        requestAndSaveData(query)
+
+        // Get data from the local cache
+        val data = cache.reposByName(query)
+
+        return RepoSearchResult(data, networkErrors)
+    }
+
+    fun requestMore(query: String) {
+        requestAndSaveData(query)
+    }
+
+    private fun requestAndSaveData(query: String) {
+        if (isRequestInProgress) return
+
+        isRequestInProgress = true
+        searchRepos(service, query, lastRequestedPage, NETWORK_PAGE_SIZE, { repos ->
+            cache.insert(repos, {
+                lastRequestedPage++
+                isRequestInProgress = false
+            })
+        }, { error ->
+            networkErrors.postValue(error)
+            isRequestInProgress = false
+        })
+    }
+
+    companion object {
+        private const val NETWORK_PAGE_SIZE = 50
+    }
+}
---
+++ app/src/main/java/com/jsandroid/paging/db/GithubLocalCache.kt
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.jsandroid.paging.db
+
+import android.content.Context
+import android.util.Log
+import androidx.lifecycle.LiveData
+import com.jsandroid.paging.model.Repo
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+
+/**
+ * Class that handles the DAO local data source. This ensures that methods are triggered on the
+ * correct executor.
+ */
+class GithubLocalCache(context: Context) {
+
+
+    private val repoDao: RepoDao = RepoDatabase.getInstance(context).reposDao()
+    private val ioExecutor: Executor = Executors.newFixedThreadPool(4)
+
+    /**
+     * Insert a list of repos in the database, on a background thread.
+     */
+    fun insert(repos: List<Repo>, insertFinished: ()-> Unit) {
+        ioExecutor.execute {
+            Log.d("GithubLocalCache", "inserting ${repos.size} repos")
+            repoDao.insert(repos)
+            insertFinished()
+        }
+    }
+
+    /**
+     * Request a LiveData<List<Repo>> from the Dao, based on a repo name. If the name contains
+     * multiple words separated by spaces, then we're emulating the GitHub API behavior and allow
+     * any characters between the words.
+     * @param name repository name
+     */
+    fun reposByName(name: String): LiveData<List<Repo>> {
+        // appending '%' so we can allow other characters to be before and after the query string
+        val query = "%${name.replace(' ', '%')}%"
+        return repoDao.reposByName(query)
+    }
+}
